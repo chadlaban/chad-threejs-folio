011f82732e1f797956fa17437f0ca9fa82820bbf
Chad <laban.chad@gmail.com>
2024-12-04 23:24:57 +0800

    Add: loading state

---
+++ src/components/ux/Loading.jsx
@@ -0,0 +1,7 @@
+// Loading for GLB Asset Component
+export const Loading = () => (
+    <div className="fixed inset-0 bg-[#000000] bg-opacity-50 flex items-center justify-center z-50">
+        <div className="w-12 h-12 rounded-full animate-spin border-8 border-dashed border-[#FAFAFA] border-t-transparent">
+        </div>
+    </div>
+);
