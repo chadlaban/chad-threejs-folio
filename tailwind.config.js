fd8b4c139519d474903651ff0a471dde7ed3a158
Chad <laban.chad@gmail.com>
2024-12-10 20:24:58 +0800

    Update: glow animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        glow: "glow 1.5s ease-in-out infinite",
+      },
+      keyframes: {
+        glow: {
+          "0%, 100%": { filter: "drop-shadow(0 0 10px #a3bf7c)" },
+          "50%": { filter: "drop-shadow(0 0 20px #a3bf7c)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
