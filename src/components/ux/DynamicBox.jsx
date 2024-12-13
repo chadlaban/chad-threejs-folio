d51b377c08b0ca33b35391f1a735101f1e5ba542
Chad <laban.chad@gmail.com>
2024-12-13 20:22:57 +0800

    Update: structure and new component

--- src/components/ux/DynamicBox.jsx
+++ src/components/ux/DynamicBox.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { clickableArr } from "../../utils/QualityOfLife";
+import MeshPage from "../../layout/MeshPage";
 
 export const DynamicBox = ({ mesh }) => {
   const [isMounted, setIsMounted] = useState(false);
@@ -20,16 +21,16 @@ export const DynamicBox = ({ mesh }) => {
 
   return (
     <div
-      className={`fixed top-0 right-0 w-1/3 h-full z-50 transition-transform duration-1000 ease-in-out ${
+      id="page"
+      className={`fixed top-0 right-0 w-5/12 h-full z-50 transition-transform duration-1000 ease-in-out opacity-95 ${
         isMounted && mesh ? "translate-x-0" : "translate-x-full"
       } ${transitionDelay ? "transition-bg duration-1000 ease-in-out" : ""}`}
       // inline style due Tailwind unable to process dynamic bg-[] colors
       style={{
         backgroundColor: object?.color || "#FAFAFA",
-        opacity: 0.6,
       }}
     >
-      <h2 className="text-white text-2xl p-4">Selected Object: {mesh}</h2>
+      <MeshPage mesh={object.mesh} />
     </div>
   );
 };
