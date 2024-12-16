01d56c17df26965ba9c31e340d525e9da74ddee8
Chad <laban.chad@gmail.com>
2024-12-16 20:42:58 +0800

    Update: fix exports

--- src/utils/CustomMesh.js
+++ src/utils/CustomMesh.js
@@ -21,4 +21,19 @@ const getColorHex = (color) => {
   return hexColor;
 };
 
-export { addBloomEffect, removeBloomEffect, getColorHex };
+const createVideoTexture = (src) => {
+  const video = document.createElement("video");
+  video.src = src;
+  video.loop = true;
+  video.muted = true;
+  video.play();
+
+  const videoTexture = new THREE.VideoTexture(video);
+  videoTexture.minFilter = THREE.LinearFilter;
+  videoTexture.magFilter = THREE.LinearFilter;
+  videoTexture.format = THREE.RGBAFormat;
+
+  return videoTexture;
+};
+
+export { addBloomEffect, removeBloomEffect, getColorHex, createVideoTexture };
