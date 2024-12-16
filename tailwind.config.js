de9997d92c3ea27be588de1f71efebeaba3b6b40
Chad <laban.chad@gmail.com>
2024-12-16 20:38:45 +0800

    Update: custom keyframe and color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,22 @@ export default {
     extend: {
       animation: {
         glow: "glow 1.5s ease-in-out infinite",
+        "slide-in": "slide-in 0.5s forwards",
       },
       keyframes: {
         glow: {
           "0%, 100%": { filter: "drop-shadow(0 0 10px #a3bf7c)" },
           "50%": { filter: "drop-shadow(0 0 20px #a3bf7c)" },
+        },
+        "slide-in": {
+          from: {
+            opacity: "0",
+            transform: "translateX(20px)",
+          },
+          to: {
+            opacity: "1",
+            transform: "translateX(0)",
+          },
         },
       },
       fontFamily: {
@@ -17,21 +28,10 @@ export default {
         js: ['"Josefin Slab"', "serif"],
         mt: ["Montserrat", "sans-serif"],
       },
-      fontSize: {
-        xs: "0.75rem",
-        sm: "0.875rem",
-        base: "1rem",
-        lg: "1.125rem",
-        xl: "1.25rem",
-        "2xl": "1.5rem",
-        "3xl": "1.875rem",
-        "4xl": "2.25rem",
-        "5xl": "3rem",
-        "6xl": "3.75rem",
-      },
       colors: {
         customGray: "#222222",
         customWhite: "#fafafa",
+        customGreen: "#a3bf7c",
       },
     },
   },
