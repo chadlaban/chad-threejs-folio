74b54cff62c0a67286674b2efae83f4141dfc6ce
Chad <laban.chad@gmail.com>
2024-12-16 20:40:07 +0800

    Update: arrays for mesh

--- src/utils/QualityOfLife.js
+++ src/utils/QualityOfLife.js
@@ -1,39 +1,114 @@
-import * as THREE from "three";
-
-const createVideoTexture = (src) => {
-  const video = document.createElement("video");
-  video.src = src;
-  video.loop = true;
-  video.muted = true;
-  video.play();
-
-  const videoTexture = new THREE.VideoTexture(video);
-  videoTexture.minFilter = THREE.LinearFilter;
-  videoTexture.magFilter = THREE.LinearFilter;
-  videoTexture.format = THREE.RGBAFormat;
-
-  return videoTexture;
-};
-
-const clickableArr = [
-  { mesh: "LargeBeanBag", color: "#9B5C44" },
-  { mesh: "OfficeChair", color: "#4E3C4C" },
-  { mesh: "Screen001", color: "#5F6C75" },
-  { mesh: "CpuTower", color: "#ACA9BB" },
-  { mesh: "MirrorCase001", color: "#FFFFFF" },
-  { mesh: "Vase", color: "#412728" },
-  { mesh: "BeautifulMe", color: "#272522" },
-];
-
 const images = [
-  { id: 4, path: "/images/memories/analyst_team.jpg" },
-  { id: 7, path: "/images/memories/giant_encompass.jpeg" },
-  { id: 8, path: "/images/memories/graduation.jpeg" },
-  { id: 3, path: "/images/memories/prof_serv_1.jpg" },
-  { id: 2, path: "/images/memories/prof_serv_2.jpg" },
-  { id: 1, path: "/images/memories/self_1.jpg" },
-  { id: 5, path: "/images/memories/valaks_analyst.webp" },
-  { id: 6, path: "/images/memories/morning_shift.jpeg" },
+  { id: 4, path: "/media/memories/analyst_team.jpg" },
+  { id: 7, path: "/media/memories/giant_encompass.jpeg" },
+  { id: 3, path: "/media/memories/prof_serv_1.jpg" },
+  { id: 2, path: "/media/memories/prof_serv_2.jpg" },
+  { id: 5, path: "/media/memories/valaks_analyst.webp" },
+  { id: 6, path: "/media/memories/morning_shift.jpeg" },
+  { id: 8, path: "/media/memories/morning_shift_2.jpg" },
+  { id: 9, path: "/media/memories/morning_shift3.jpg" },
 ];
 
-export { createVideoTexture, clickableArr, images };
+const hobbiesMedia = [
+  {
+    id: 1,
+    type: "image",
+    src: "/media/hobbies/butt_fly.jpg",
+    alt: "hobby_img",
+  },
+  {
+    id: 2,
+    type: "video",
+    src: "/media/hobbies/apex_gay.mp4",
+    alt: "hobby_mp4",
+  },
+  { id: 3, type: "image", src: "/media/hobbies/moa_gay.jpg", alt: "hobby_img" },
+  {
+    id: 4,
+    type: "video",
+    src: "/media/hobbies/competitive_friendly.mp4",
+    alt: "hobby_mp4",
+  },
+  {
+    id: 5,
+    type: "image",
+    src: "/media/hobbies/natures_child.jpg",
+    alt: "hobby_img",
+  },
+  {
+    id: 6,
+    type: "video",
+    src: "/media/hobbies/amazing_cw.mp4",
+    alt: "hobby_mp4",
+  },
+];
+
+const selectableObjects = [
+  {
+    id: 1,
+    title: "About Me",
+    path: "/media/selectables/DesktopMonitor.jpg",
+    mesh: "Screen001",
+    color: "#5F6C75",
+    textColor: "#1D1D1D",
+  },
+  {
+    id: 2,
+    title: "Employment",
+    path: "/media/selectables/OfficeChair.jpg",
+    mesh: "OfficeChair",
+    color: "#4E3C4C",
+    textColor: "#FAFAFA",
+  },
+  {
+    id: 3,
+    title: "Projects",
+    path: "/media/selectables/CpuTower.jpg",
+    mesh: "CpuTower",
+    color: "#ACA9BB",
+    textColor: "#1D1D1D",
+  },
+  {
+    id: 4,
+    title: "Certifications",
+    path: "/media/selectables/BonsaiCase.jpg",
+    mesh: "MirrorCase001",
+    color: "#FAFAFA",
+    textColor: "#1D1D1D",
+  },
+  {
+    id: 5,
+    title: "Teams",
+    path: "/media/selectables/Picture.jpg",
+    mesh: "BeautifulMe",
+    color: "#272522",
+    textColor: "#FAFAFA",
+  },
+  {
+    id: 6,
+    title: "Hobbies",
+    path: "/media/selectables/MaroonVase.jpg",
+    mesh: "Vase",
+    color: "#412728",
+    textColor: "#FAFAFA",
+  },
+  {
+    id: 7,
+    title: "Contact Me",
+    path: "/media/selectables/BeanBag.jpg",
+    mesh: "LargeBeanBag",
+    color: "#9B5C44",
+    textColor: "#1D1D1D",
+  },
+];
+
+const aboutMeParaLines = [
+  "Hello! I'm Chad Laban, a passionate Software Developer with a love for crafting subtle but impactful user experiences.",
+  "In my recent role at 株式会社 ゴールドバリュークリエーション (Golden Value Creation), I worked on the Okimeguri project, handling both frontend and backend development tasks. I honed my skills in code implementation, debugging, and collaboration with fellow developers.",
+  "Previously, at Capella BPO, I worked on client-side web applications using modern frameworks and created a test server for endpoint creation. Before that, at Giant International Software Station Inc., I handled feature requests and dashboard customizations, further diversifying my skill set.",
+  "I value the camaraderie and support from my colleagues, which made every day an opportunity for growth and collaboration. My experience as a Software Analyst at Giant International Software Station enhanced my vocal confidence through customer support and reignited my passion for software development.",
+  "I love learning from people and strive for quality outputs in my work. I enjoy working with teams and learning from them. Outside work, I play FPS and single-player games, cook meals, jog occasionally, and am starting a new hobby of reading books.",
+  "I look forward to connecting with fellow professionals, sharing insights, and exploring opportunities to collaborate and learn. Feel free to reach out—I'm always up for a good tech discussion or a new project venture. Let's connect!",
+];
+
+export { images, hobbiesMedia, aboutMeParaLines, selectableObjects };
