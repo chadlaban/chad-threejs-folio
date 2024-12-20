db67a4c61c20eaace38b5bcd2464992aa53ee1e0
Chad <laban.chad@gmail.com>
2024-12-20 19:32:27 +0800

    Update: applying motion

--- src/components/ui/ExpandableString.jsx
+++ src/components/ui/ExpandableString.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import PropTypes from "prop-types";
 import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/outline";
 import { truncateText } from "../../utils/String";
+import { motion } from "framer-motion";
 
 const ExpandableString = ({ text, limit }) => {
   const [isExpanded, setIsExpanded] = useState(false);
@@ -11,11 +12,26 @@ const ExpandableString = ({ text, limit }) => {
   };
 
   return (
-    <>
-      {isExpanded ? text : truncateText(text, limit)}
-      <button
+    <motion.div>
+      {/* displayed text by limit */}
+      {!isExpanded && <p>{truncateText(text, limit)}</p>}
+
+      <motion.div
+        initial={{ opacity: 0, height: 0 }}
+        animate={{
+          opacity: 1,
+          height: isExpanded ? "auto" : 0,
+        }}
+        transition={{ duration: 0.3 }}
+        style={{ overflow: "hidden" }}
+      >
+        {isExpanded ? <div>{text}</div> : <p>{""}</p>}
+      </motion.div>
+
+      {/* button trigger */}
+      <motion.button
         onClick={toggleExpand}
-        className="font-js text-sm text-blue-500 ml-1 cursor-pointer"
+        className="font-js text-sm text-customGray ml-1 cursor-pointer"
       >
         {isExpanded ? (
           <>
@@ -26,8 +42,8 @@ const ExpandableString = ({ text, limit }) => {
             <ChevronDownIcon className="w-3 h-3" />
           </>
         )}
-      </button>
-    </>
+      </motion.button>
+    </motion.div>
   );
 };
 
